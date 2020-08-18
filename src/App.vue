bd65acd8281b6aa6896f9ddae819082a03948633
dingmingfeng <dingmingfeng@bbktel.com>
2020-08-18 20:47:36 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+	<div id="app">
+		<bbs-editor />
+	</div>
+</template>
+
+<script>
+import BBSEditor from '@/component/bbs-editor'
+
+export default {
+	name: 'App',
+	components: {
+		'bbs-editor': BBSEditor
+	}
+}
+</script>
