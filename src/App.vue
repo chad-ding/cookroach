79f6ebd33c5eb48ab428ab4f1d823a57cad56f06
dingmingfeng <dingmingfeng@bbktel.com>
2020-08-20 10:06:50 +0800

    save

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,56 @@
 <template>
 	<div id="app">
-		<ck-editor v-if="editorType === 'ck'" />
-		<fr-editor v-else />
+		<div>
+			<ck-editor v-if="editorType === 'ck'" />
+			<tm-editor v-if="editorType === 'tm'" />
+			<fr-editor v-else />
+		</div>
+		<div>
+			<button class="btn">提交</button>
+		</div>
 	</div>
 </template>
+
+<style lang="less" scoped>
+.btn {
+	color: #fff;
+	background-color: #337ab7;
+	border-color: #2e6da4;
+	margin-top: 5px;
+	margin-bottom: 5px;
+	display: inline-block;
+	padding: 6px 12px;
+	margin-bottom: 0;
+	font-size: 14px;
+	font-weight: 400;
+	line-height: 1.42857143;
+	text-align: center;
+	white-space: nowrap;
+	vertical-align: middle;
+	touch-action: manipulation;
+	cursor: pointer;
+	user-select: none;
+	background-image: none;
+	border: 1px solid transparent;
+	border-radius: 4px;
+}
+</style>
 
 <script>
 import CKEditor from '@/ck-editor'
 import FREditor from '@/fr-editor'
+import TMEditor from '@/tm-editor'
 
 export default {
 	name: 'App',
 	components: {
 		'ck-editor': CKEditor,
-		'fr-editor': FREditor
+		'fr-editor': FREditor,
+		'tm-editor': TMEditor
 	},
 	data() {
 		return {
-			editorType: 'fr'
+			editorType: 'tm'
 		}
 	}
 }
---
+++ src/tm-editor/index.vue
@@ -0,0 +1,28 @@
+<template>
+	<div>
+		<editor ref="editor" api-key="no-api-key" :init="editorConf" />
+	</div>
+</template>
+
+<script>
+import Editor from '@tinymce/tinymce-vue'
+
+export default {
+	components: {
+		editor: Editor
+	},
+	data() {
+		return {
+			editorConf: {
+				height: 500,
+				menubar: false,
+				plugins: ['advlist autolink lists link image charmap print preview anchor', 'searchreplace visualblocks code fullscreen', 'insertdatetime media table paste code help wordcount'],
+				toolbar: 'bold italic color | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | removeformat | help'
+			}
+		}
+	},
+	mounted() {
+		console.log(this.$refs.editor)
+	}
+}
+</script>
